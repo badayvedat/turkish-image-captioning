249d1b666b22289b5458526b7c8b8f9617a630e5
badayvedat <badayvedat@gmail.com>
2021-07-30 22:33:10 +0300

    download script fixed

--- download_pretrained.sh
+++ download_pretrained.sh
@@ -1,7 +1,9 @@
 # Since It's my drive account I do not guarantee that this link will work in the future.
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1WftjS5CvGItebD9Vav7oHG7I1eBIpmLx' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1WftjS5CvGItebD9Vav7oHG7I1eBIpmLx" -O 12heads.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1O_gPAOKNrAPgloHV5CMQKRNlGi-Kvg-f' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1O_gPAOKNrAPgloHV5CMQKRNlGi-Kvg-f" -O 12heads.zip && rm -rf /tmp/cookies.txt
 
 unzip -q 12heads.zip -d 12heads
 mv 12heads/checkpoints/* checkpoints/
-mv *.pkl ./
+mv 12heads/*.pkl ./
+mv 12heads/configs/* configs/
 rm -dr 12heads
+rm 12heads.zip
