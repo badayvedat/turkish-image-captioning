2b3304e35ca8466633679496967a869f0124888b
Vedat Baday <54285744+badayvedat@users.noreply.github.com>
2021-08-02 18:11:56 +0300

    fix download pretrained script

--- download_pretrained.sh
+++ download_pretrained.sh
@@ -2,6 +2,7 @@
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1O_gPAOKNrAPgloHV5CMQKRNlGi-Kvg-f' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1O_gPAOKNrAPgloHV5CMQKRNlGi-Kvg-f" -O 12heads.zip && rm -rf /tmp/cookies.txt
 
 unzip -q 12heads.zip -d 12heads
+mkdir -p checkpoints
 mv 12heads/checkpoints/* checkpoints/
 mv 12heads/*.pkl ./
 mv 12heads/configs/* configs/
